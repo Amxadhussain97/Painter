b9c627b02ef7c140e09f915585d33b3988d2ef3e
Amxadhussain97 <amxadhussain97@gmail.com>
2021-12-10 11:13:18 +0600

    newerror

--- app/Http/Middleware/userMiddleware.php
+++ app/Http/Middleware/userMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class userMiddleware
 {
@@ -18,6 +19,21 @@ class userMiddleware
     public function handle(Request $request, Closure $next)
     {
 
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+
+            return Response::make('OK', 200, $headers);
+        }
+
+
         $userId = auth()->user()->id;
         $user = User::where('id', $userId)->first();
 
@@ -25,9 +41,7 @@ class userMiddleware
             if ($user->role != 'Admin') {
                 $request->user_id = $userId;
             }
-        }
-
-        else $request->user_id = $userId;
+        } else $request->user_id = $userId;
 
 
 
