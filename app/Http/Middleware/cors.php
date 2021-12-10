3784801f62ff60ae205c0d379fed0705c016ace6
Amxadhussain97 <amxadhussain97@gmail.com>
2021-12-10 11:55:29 +0600

    jjjj

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use Illuminate\Http\Request;
 use Closure;
 use JWTAuth;
@@ -19,8 +20,7 @@ class cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
